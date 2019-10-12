2f939bd350c39d840bdbb1a29af060c0b145afbc
taba <tabamlak@gmail.com>
2019-10-12 18:23:42 +0200

    m2

---
+++ app/src/main/java/com/example/wedding_hall/harith/Details.java
@@ -0,0 +1,16 @@
+package com.example.wedding_hall.harith;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wedding_hall.R;
+
+public class Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detales);
+
+    }
+}
---
+++ app/src/main/java/com/example/wedding_hall/harith/Splash.java
@@ -0,0 +1,15 @@
+package com.example.wedding_hall.harith;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wedding_hall.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
